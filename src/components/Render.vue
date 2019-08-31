97859599d82bfccb542ab9b287cdf0b7bd5b72f9
Guillaume Ader <guillaume@172-0-10-61.lightspeed.rcsntx.sbcglobal.net>
2019-08-31 18:02:35 +0100

    dispaly renderer

--- src/components/Render.vue
+++ src/components/Render.vue
@@ -9,5 +9,12 @@ export default {
   props: {
     colors: Array
   },
+  mounted() {
+    // this.refs.canvas.
+    for(let i in this.colors) {
+      this.$refs.canvas.fillStyle = `rgb(${this.colors[i][0]}, ${this.colors[i][1]}, ${this.colors[i][2]})`
+      this.$refs.canvas.fillRect(res[i].x, res[i].y, this.cellWidth, this.cellHeight)
+    }
+  }
 }
 </script>
